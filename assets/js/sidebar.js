ec316ddbf6bace86f96416b51068dae7109dfd42
WoojinJeonkr <wooeric1@naver.com>
2023-12-03 00:06:03 +0900

    fix: resize sidebar size

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,12 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
   const sidebar = document.querySelector(".sidebar");
+  const initialTop = 700;
 
   if (sidebar) {
     window.addEventListener("scroll", function () {
       const scrollY = window.scrollY;
-
-      // 조절 가능한 스크롤 위치에 따른 사이드바의 상단 위치
-      const sidebarTop = scrollY < 200 ? 200 - scrollY : 0;
+      const sidebarTop = Math.max(initialTop - scrollY, 0);
 
       sidebar.style.top = `${sidebarTop}px`;
     });
