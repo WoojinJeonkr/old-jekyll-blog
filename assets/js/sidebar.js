bd7f7a617cd5663b1ae8aea375bf29937f79dd5d
WoojinJeonkr <wooeric1@naver.com>
2023-12-03 17:42:30 +0900

    fix: simplify tags displayed in the sidebar

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,6 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
   const sidebar = document.querySelector(".sidebar");
   const initialTop = 700;
+  const maxHeight = 500; // 최대 높이 값
+
+  function adjustSidebarHeight() {
+    const contentHeight = document.body.scrollHeight;
+    const newHeight = Math.min(contentHeight, maxHeight);
+    sidebar.style.height = `${newHeight}px`;
+  }
 
   if (sidebar) {
     window.addEventListener("scroll", function () {
@@ -8,6 +15,12 @@ document.addEventListener("DOMContentLoaded", function () {
       const sidebarTop = Math.max(initialTop - scrollY, 0);
 
       sidebar.style.top = `${sidebarTop}px`;
+
+      // 추가: 세로 길이 동적 조절
+      adjustSidebarHeight();
     });
+
+    // 페이지 로드 시 세로 길이 조절
+    adjustSidebarHeight();
   }
 });
